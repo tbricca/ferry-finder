843e9ea929f3d689dc4f91f98045a5063f80d905
tbricca <tbricca@gmail.com>
2018-02-05 08:33:25 -0800

    restyling landing page

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const slider = document.querySelector('.slider');
 
+
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+});
+
 $(document).ready(function () {
     $('.slider').slider();
 });
